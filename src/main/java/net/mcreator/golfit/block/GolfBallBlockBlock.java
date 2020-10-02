5f4e03e252daac5720fed8c40bdf88f00cad262a
jpgoncalves56@gmail.com <jpgoncalves56@gmail.com>
2020-10-02 17:46:50 +0100

    Added a 3d ball model

--- src/main/java/net/mcreator/golfit/block/GolfBallBlockBlock.java
+++ src/main/java/net/mcreator/golfit/block/GolfBallBlockBlock.java
@@ -2,13 +2,18 @@
 package net.mcreator.golfit.block;
 
 import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
 
 import net.minecraft.world.storage.loot.LootContext;
-import net.minecraft.world.World;
+import net.minecraft.world.IBlockReader;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Item;
 import net.minecraft.item.BlockItem;
+import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.BlockState;
@@ -17,9 +22,7 @@ import net.minecraft.block.Block;
 import net.mcreator.golfit.itemgroup.GolfItemGroup;
 import net.mcreator.golfit.GolfItModElements;
 
-import java.util.Map;
 import java.util.List;
-import java.util.HashMap;
 import java.util.Collections;
 
 @GolfItModElements.ModElement.Tag
@@ -35,10 +38,26 @@ public class GolfBallBlockBlock extends GolfItModElements.ModElement {
 		elements.blocks.add(() -> new CustomBlock());
 		elements.items.add(() -> new BlockItem(block, new Item.Properties().group(GolfItemGroup.tab)).setRegistryName(block.getRegistryName()));
 	}
+
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	public void clientLoad(FMLClientSetupEvent event) {
+		RenderTypeLookup.setRenderLayer(block, RenderType.getCutout());
+	}
 	public static class CustomBlock extends Block {
 		public CustomBlock() {
-			super(Block.Properties.create(Material.MISCELLANEOUS).sound(SoundType.ANVIL).hardnessAndResistance(1f, 10f).lightValue(0));
+			super(Block.Properties.create(Material.MISCELLANEOUS).sound(SoundType.CLOTH).hardnessAndResistance(1f, 10f).lightValue(0).notSolid());
 			setRegistryName("golf_ball_block");
+		}
+
+		@Override
+		public boolean isNormalCube(BlockState state, IBlockReader worldIn, BlockPos pos) {
+			return false;
+		}
+
+		@Override
+		public boolean propagatesSkylightDown(BlockState state, IBlockReader reader, BlockPos pos) {
+			return true;
 		}
 
 		@Override
@@ -47,21 +66,6 @@ public class GolfBallBlockBlock extends GolfItModElements.ModElement {
 			if (!dropsOriginal.isEmpty())
 				return dropsOriginal;
 			return Collections.singletonList(new ItemStack(this, 1));
-		}
-
-		@Override
-		public void onBlockAdded(BlockState state, World world, BlockPos pos, BlockState oldState, boolean moving) {
-			super.onBlockAdded(state, world, pos, oldState, moving);
-			int x = pos.getX();
-			int y = pos.getY();
-			int z = pos.getZ();
-			{
-				Map<String, Object> $_dependencies = new HashMap<>();
-				$_dependencies.put("x", x);
-				$_dependencies.put("y", y);
-				$_dependencies.put("z", z);
-				$_dependencies.put("world", world);
-			}
 		}
 	}
 }
